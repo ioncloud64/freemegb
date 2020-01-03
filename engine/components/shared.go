3c9da898455e3456b2be9909d2fb61d6846b504c
ioncloud64 <admin@ioncloud64.com>
2020-01-02 23:31:36 -0600

    Minor update Created license file Modified logfile location

--- engine/components/shared.go
+++ engine/components/shared.go
@@ -3,18 +3,28 @@ package components
 import (
 	"log"
 	"os"
+	"path"
 	"strings"
 	"time"
 
 	"github.com/gotk3/gotk3/gtk"
 )
 
-var LogFilename = strings.ReplaceAll("freemegb_"+time.Now().Format("January 2, 2006")+".log", " ", "_")
+var UserHome, err = os.UserHomeDir()
 
-var LogFile, err = os.OpenFile(LogFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+var LogFilename string
 
-var Logger = log.New(LogFile, "FreeMe!GB ", log.LstdFlags)
+var LogFile *os.File
+
+var Logger *log.Logger
 
 var ROMref []byte
 
 var AppRef *gtk.Application
+
+func Init() {
+	os.MkdirAll(path.Join(UserHome,".freemegb"), os.FileMode(0755))
+	LogFilename = UserHome+"/.freemegb/"+strings.ReplaceAll("freemegb_"+time.Now().Format("January 2, 2006")+".log", " ", "_")
+	LogFile, err = os.OpenFile(LogFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	Logger = log.New(LogFile, "FreeMe!GB ", log.LstdFlags)
+}
